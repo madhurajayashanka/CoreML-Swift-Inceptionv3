d53a465ee0640cde078bbe8d84475323da3c1cb8
Madhura <madhuraweerasooriye@gmail.com>
2023-01-13 22:01:15 +0530

    initial commit

---
+++ FoodRecognize/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  FoodRecognize
+//
+//  Created by Madhura on 2023-01-13.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var objectLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = userPickedImage
+            guard let ciimage = CIImage(image: userPickedImage) else{
+                fatalError("Image fail")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true)
+    }
+    
+    func detect(image: CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("error")
+        }
+        let request = VNCoreMLRequest(model: model){(request,error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("error")
+            }
+            if let firstResult = results.first?.identifier.components(separatedBy: ",")[0]{
+                self.objectLabel.text = firstResult
+                
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
+    }
+    
+}
+
